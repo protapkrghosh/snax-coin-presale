a2caadc9913e8c4b020b865bb9ef3c89abb4631d
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-18 00:50:45 +0600

    aa

--- src/components/Sections/AboutOrFeatures/AboutOrFeatures.jsx
+++ src/components/Sections/AboutOrFeatures/AboutOrFeatures.jsx
@@ -16,7 +16,7 @@ const AboutOrFeatures = () => {
           {/* --------------- About section --------------- */}
           <div className='flex justify-center items-center lg:gap-[70px]'>
             {/* --------------- about image section ------------- */}
-            <div className='w-[50%]'>
+            <div className='w-[48%]'>
 
               {/* about image section shadow */}
               <img className='absolute 2xl:w-[12%] lg:w-auto 2xl:top-[-100px] 2xl:left-[100px]' src={aboutImgleftShadow} alt="image" />
@@ -24,18 +24,18 @@ const AboutOrFeatures = () => {
               <img className='2xl:w-auto' src={aboutImg} alt="image" />
             </div>
             {/* --------------- about contain info section ------------- */}
-            <div className='w-[50%]'>
+            <div className='w-[52%]'>
 
               {/* about contain section shadow */}
-              <img className='absolute 2xl:w-[30%] lg:w-auto 2xl:top-[-240px] 2xl:right-[540px]' src={aboutImgRightShadow} alt="image" />
+              <img className='absolute 2xl:w-[30%] lg:w-auto 2xl:top-[-240px] 2xl:right-[570px]' src={aboutImgRightShadow} alt="image" />
 
               <div className='border-[3px] border-dashed lg:rounded-[30px] lg:pl-[50px]'>
                 <div>
                   <p className='xl:mt-[60px] xl:text-[80px] lg:leading-[80px] lg:tracking-[2.4px]'><span className='text-[#FFF]'>About</span> snax</p>
 
-                  <p className='lg:mt-[30px] xl:w-[85%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>In the enchanting narrative of Snax, the crypto-savvy pup, lies the genesis of Snax Coin, a beacon of innovation in the blockchain realm. Snax Coin's mission is to democratize access to financial opportunities worldwide, fostering a community-driven ecosystem where individuals can transact securely and transparently.</p>
+                  <p className='lg:mt-[30px] 2xl:w-[92%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>In the enchanting narrative of Snax, the crypto-savvy pup, lies the genesis of Snax Coin, a beacon of innovation in the blockchain realm. Snax Coin's mission is to democratize access to financial opportunities worldwide, fostering a community-driven ecosystem where individuals can transact securely and transparently.</p>
 
-                  <p className='lg:mt-[30px] xl:w-[97%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>Visionary and inclusive, Snax Coin aims to reshape the future of finance, empowering users to navigate the digital landscape with confidence. Backed by a dynamic team of cryptocurrency veterans boasting expertise in blockchain technology, finance, and community development, Snax Coin stands poised to revolutionize the crypto space with integrity and purpose.</p>
+                  <p className='lg:mt-[30px] 2xl:w-[94%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>Visionary and inclusive, Snax Coin aims to reshape the future of finance, empowering users to navigate the digital landscape with confidence. Backed by a dynamic team of cryptocurrency veterans boasting expertise in blockchain technology, finance, and community development, Snax Coin stands poised to revolutionize the crypto space with integrity and purpose.</p>
 
                   <Button className="lg:mt-[50px] lg:mb-[60px] lg:text-[20px] lg:px-[50px] lg:py-[16px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] hover:from-[#f8b415ec] hover:to-[#0fc1c7e8] lg:leading-[30px] lg:tracking-[0.8px] text-[#000] rounded-[10px] duration-300 w-full md:w-fit">More about us</Button>
 
