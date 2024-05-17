0b920aed79645587d4ecebe86f96d0a1afa92805
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-17 21:58:41 +0600

    aa

--- src/components/Sections/AboutOrFeatures/AboutOrFeatures.jsx
+++ src/components/Sections/AboutOrFeatures/AboutOrFeatures.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../Container/Container';
 import aboutImg from '@/assets/about/Group 1171275232 (1).png';
+import { Button } from '@/components/ui/button';
 
 const AboutOrFeatures = () => {
   return (
@@ -9,16 +10,23 @@ const AboutOrFeatures = () => {
       <Container>
 
         {/* --------------- About section --------------- */}
-        <div>
+        <div className='flex justify-center items-center lg:gap-[70px]'>
           {/* --------------- about image section ------------- */}
           <div className='w-[50%]'>
             <img className='2xl:w-auto' src={aboutImg} alt="image" />
           </div>
           {/* --------------- about contain info section ------------- */}
           <div className='w-[50%]'>
-            <div>
+            <div className='border-[3px] border-dashed lg:rounded-[30px] lg:pl-[50px]'>
               <div>
-                <p className='xl:text-[80px] lg:leading-[80px] lg:tracking-[2.4px]'><span className='text-[#FFF]'>About</span> snax</p>
+                <p className='xl:mt-[60px] xl:text-[80px] lg:leading-[80px] lg:tracking-[2.4px]'><span className='text-[#FFF]'>About</span> snax</p>
+
+                <p className='lg:mt-[30px] xl:w-[85%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>In the enchanting narrative of Snax, the crypto-savvy pup, lies the genesis of Snax Coin, a beacon of innovation in the blockchain realm. Snax Coin's mission is to democratize access to financial opportunities worldwide, fostering a community-driven ecosystem where individuals can transact securely and transparently.</p>
+
+                <p className='lg:mt-[30px] xl:w-[97%] text-[#E5DADA] lg:text-[20px] lg:leading-[36px] lg:tracking-[0.8px]'>Visionary and inclusive, Snax Coin aims to reshape the future of finance, empowering users to navigate the digital landscape with confidence. Backed by a dynamic team of cryptocurrency veterans boasting expertise in blockchain technology, finance, and community development, Snax Coin stands poised to revolutionize the crypto space with integrity and purpose.</p>
+
+                <Button className="lg:mt-[50px] lg:mb-[60px] lg:text-[20px] lg:px-[50px] lg:py-[16px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] hover:from-[#f8b415ec] hover:to-[#0fc1c7e8] lg:leading-[30px] lg:tracking-[0.8px] text-[#000] rounded-[10px] duration-300 w-full md:w-fit">More about us</Button>
+
               </div>
             </div>
           </div>
