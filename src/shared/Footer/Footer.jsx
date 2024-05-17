0a2590be9b62678984df4342d6428ef53a121dce
Protap Ghosh <protapg518@gmail.com>
2024-05-17 22:35:07 +0600

    footer complete

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 import img1 from '@/assets/footer/Dinosaur-art.png';
 import img2 from '@/assets/footer/Dinosaur-bone.png';
 import logo from '@/assets/banner/Frame 431.png';
-import { MdOutlineArrowOutward } from "react-icons/md";
+import icon1 from '@/assets/footer/Mask group.png';
+import icon2 from '@/assets/footer/Group 8773.png';
 import { FaTelegramPlane, FaRedditAlien } from "react-icons/fa";
 import { FaXTwitter, FaInstagram } from "react-icons/fa6";
 
@@ -31,13 +32,13 @@ const Footer = () => {
 
             <a href="" target='_blank' className='text-white'>
               <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-4 rounded-full duration-300'>
-                <FaInstagram className='text-[25px]' />
+                <img src={icon1} alt="Image" className='w-[24px] h-[25px]' />
               </div>
             </a>
 
             <a href="" target='_blank' className='text-white'>
-              <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-4 rounded-full duration-300'>
-                <FaRedditAlien className='text-[25px]' />
+              <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-[15px] 2xl:p-4 rounded-full duration-300'>
+                <img src={icon2} alt="Image" className='w-[26px]'/>
               </div>
             </a>
           </div>
