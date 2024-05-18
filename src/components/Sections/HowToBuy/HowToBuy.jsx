99f2dea73cf539b042c8ed9741b0f27635d20ad5
Protap Ghosh <protapg518@gmail.com>
2024-05-18 17:11:59 +0600

    added how to buy section

---
+++ src/components/Sections/HowToBuy/HowToBuy.jsx
@@ -0,0 +1,98 @@
+import cloud from '@/assets/meme-Ge4nerator/Group 1171275247.png';
+import grass from '@/assets/howToBuy/Group 1171275261.png';
+import shape from '@/assets/ourWayToTheMoon/Group 1171275272.png';
+import vector1 from '@/assets/howToBuy/Vector.png';
+import vector2 from '@/assets/howToBuy/Group.png';
+import dog from '@/assets/howToBuy/logo_short_no_bg_square.png';
+import dogBone from '@/assets/howToBuy/Group 1171275272.png';
+import cardGrass from '@/assets/howToBuy/Group 1171275247.png';
+import Container from '../Container/Container';
+
+const HowToBuy = () => {
+  return (
+    <div className='mt-[138px] mb-[80px]'>
+      <Container>
+        {/* Buy heading */}
+        <div className='flex justify-center mb-5 md:mb-10 lg:mb-0'>
+          <div className='flex mt-5'>
+            <img src={cloud} alt="Image" className='w-[70px] md:w-[170px] lg:w-[200px] xl:w-auto h-[95px] md:h-auto' />
+            <h1 className='text-white text-[30px] md:text-[45px] lg:text-[60px] xl:text-[80px] text-center lg:leading-[88px] lg:tracking-[2.4px] mt-3 md:mt-5 lg:mt-5 xl:mt-10 ml-[-55px] md:ml-[-105px] lg:ml-[-140px] xl:ml-[-145px]'>How to <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>buy?</span></h1>
+          </div>
+          <img src={grass} alt="Image" className='h-fit ml-4 md:ml-10 w-[35px] md:w-[45px] lg:w-[80px] xl:w-[170px]' />
+        </div>
+
+        
+        {/* How to buy content */}
+        <div>
+          <div className='lg:flex justify-between gap-x-[200px]'>
+            <div className='lg:w-[50%] space-y-[68px]'>
+              {/* Create Wallet */}
+              <div>
+                <div className='z-50'>
+                  <img src={dog} alt="Image" className='mx-auto mb-[-10px]' />
+                </div>
+
+                <div className='p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit  z-10'>
+                  <div className='bg-[#18534F] rounded-[30px] p-[40px]'>
+                    <h3 className='text-white text-[28px] lg:leading-[33.6px] lg:tracking-[0.84px]'>Create <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>Wallet</span></h3>
+
+                    <p className='text-[#E7D7CB] text-[18px] lg:leading-[28.8px] lg:tracking-[0.54px] mt-[15px] w-[]'>Download metamask from the app store or google play store for free. Desktoo users, download the aocale chrome I</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Go to PancakeSwap */}
+              <div>
+                <div className='z-50'>
+                  <img src={dog} alt="Image" className='mx-auto mb-[-10px]' />
+                </div>
+
+                <div className='p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit  z-10'>
+                  <div className='bg-[#18534F] rounded-[30px] p-[40px]'>
+                    <h3 className='text-white text-[28px] lg:leading-[33.6px] lg:tracking-[0.84px]'>Go to <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>PancakeSwap</span></h3>
+
+                    <p className='text-[#E7D7CB] text-[18px] lg:leading-[28.8px] lg:tracking-[0.54px] mt-[15px] w-[]'>Connect to PancakeSwap. Go to PancakeSwap in google chrome or on the browser inside wour lictamark ago. Connect your wallet. Paste the sKillSolana BNB token Smart Contract address (0x4eCd50E&8507204D487555c59851755EF9fFAF9) into PoncokeSwoo, select TWEETY. and confirm. When. metamonk promote uou for a wollet sanature, sion.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='lg:w-[50%] space-y-[130px] mt-[183px]'>
+              {/* Get some BNB */}
+              <div>
+                <div className='z-50'>
+                  <img src={dog} alt="Image" className='mx-auto mb-[-10px]' />
+                </div>
+
+                <div className='p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit  z-10'>
+                  <div className='bg-[#18534F] rounded-[30px] p-[40px]'>
+                    <h3 className='text-white text-[28px] lg:leading-[33.6px] lg:tracking-[0.84px]'>Get some <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>BNB</span></h3>
+
+                    <p className='text-[#E7D7CB] text-[18px] lg:leading-[28.8px] lg:tracking-[0.54px] mt-[15px] w-[]'>Have BNB in your wallet to switch to skillSolona. If you don't have any BNB, you con buy directly on metamosk, transfer from another wollet, or buy on another exchange and send it to your wollet.</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Swap BNB for KillSolana */}
+              <div>
+                <div className='z-50'>
+                  <img src={dog} alt="Image" className='mx-auto mb-[-10px]' />
+                </div>
+
+                <div className='p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit  z-10'>
+                  <div className='bg-[#18534F] rounded-[30px] p-[40px]'>
+                    <h3 className='text-white text-[28px] lg:leading-[33.6px] lg:tracking-[0.84px]'>Swap BNB for <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>KillSolana</span></h3>
+
+                    <p className='text-[#E7D7CB] text-[18px] lg:leading-[28.8px] lg:tracking-[0.54px] mt-[15px] w-[]'>Switch BNB for #KillSolana. We have ZERO toxes so you don't need to worry about buying with a specific slippage, although you may need to use slippage during times of market volatility. HOLD KillSolana BNB for 1000x ATH Growth + s</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default HowToBuy;
