e15b7bbf4bd4c770451208cd5c2a2ce017d04134
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-20 05:18:54 +0600

    aa

--- src/components/Sections/HowToBuy/HowToBuy.jsx
+++ src/components/Sections/HowToBuy/HowToBuy.jsx
@@ -28,7 +28,7 @@ const HowToBuy = () => {
               <div className='2xl:w-[41%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] rounded-[30px]'>
                   <p className='text-[#FFF] text-[28px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Create <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>Wallet</span></p>
-                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px]'>Download metamask from the app store or google play store for free. Desktoo users, download the aocale chrome I</p>
+                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:leading-[28.8px] xl:tracking-[0.54px]'>Download metamask from the app store or google play store for free. Desktoo users, download the aocale chrome I</p>
                 </div>
               </div>
               {/* card shadow image  */}
@@ -45,7 +45,7 @@ const HowToBuy = () => {
               <div className='2xl:w-[100%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] rounded-[30px]'>
                   <p className='text-[#FFF] text-[28px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Get some <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>BNB</span></p>
-                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px]'>Have BNB in your wallet to switch to skillSolona. If you don't have any BNB, you con buy directly on metamosk, transfer from another wollet, or buy on another exchange and send it to your wollet.</p>
+                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:leading-[28.8px] xl:tracking-[0.54px]'>Have BNB in your wallet to switch to skillSolona. If you don't have any BNB, you con buy directly on metamosk, transfer from another wollet, or buy on another exchange and send it to your wollet.</p>
                 </div>
               </div>
               {/* card shadow image  */}
@@ -57,11 +57,31 @@ const HowToBuy = () => {
 
 
 
-            {/* ------------ main images ----------- */}
+            {/* ---------------- main images ---------------- */}
             <div className='2xl:mt-[-560px]'>
               <img className='mx-auto 2xl:w-[54%]' src={howToImg} alt="image" />
             </div>
 
+
+
+
+
+            {/* ----------- card 3 --------- */}
+            <div className='2xl:mt-[-860px] 2xl:ml-[4px] 2xl:space-y-[-10px]'>
+              <img className='2xl:ml-[240px] 2xl:w-auto' src={cardImage} alt="image" />
+
+              <div className='2xl:w-[43%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+                <div className='bg-[#18534f] 2xl:p-[40px] rounded-[30px]'>
+                  <p className='text-[#FFF] text-[28px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Go to <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>PancakeSwap</span></p>
+                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:leading-[28.8px] xl:tracking-[0.54px]'>Connect to PancakeSwap. Go to PancakeSwap in google chrome or on the browser inside wour lictamark ago. Connect your wallet. Paste the sKillSolana BNB token Smart Contract address (0x4eCd50E&8507204D487555c59851755EF9fFAF9) into PoncokeSwoo, select TWEETY. and confirm. When. metamonk promote uou for a wollet sanature, sion.</p>
+                </div>
+              </div>
+              {/* card shadow image  */}
+              <div>
+                <img className='2xl:mt-[-220px] 2xl:ml-[420px] 2xl:w-auto' src={cardImageShadow} alt="image" />
+              </div>
+
+            </div>
 
 
 
