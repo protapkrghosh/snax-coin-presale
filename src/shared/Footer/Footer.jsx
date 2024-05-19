8c60507f447966d9137511a3cd2882f4f8254b94
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-19 14:58:45 +0600

    aa

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -14,31 +14,35 @@ const Footer = () => {
 
         {/* Social media action buttons */}
         <div className='mb-7'>
-          <img src={logo} alt="Image" className='mx-auto'/>
+          <img src={logo} alt="Image" className='mx-auto' />
 
           {/* Social media action button */}
           <div className='flex justify-center items-center space-x-7 lg:space-x-7 xl:space-x-12 my-5 lg:mt-7 lg:mb-10'>
-            <a href="" target='_blank' className='text-white'>
+            <a href="" target='_blank' className='text-white' data-aos="fade-up"
+              data-aos-duration="800">
               <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-4 rounded-full duration-300'>
                 <FaTelegramPlane className='text-[25px]' />
               </div>
             </a>
 
-            <a href="" target='_blank' className='text-white'>
+            <a href="" target='_blank' className='text-white' data-aos="fade-up"
+              data-aos-duration="1100">
               <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-4 rounded-full duration-300'>
                 <FaXTwitter className='text-[25px]' />
               </div>
             </a>
 
-            <a href="" target='_blank' className='text-white'>
+            <a href="" target='_blank' className='text-white' data-aos="fade-up"
+              data-aos-duration="1400">
               <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-4 rounded-full duration-300'>
                 <img src={icon1} alt="Image" className='w-[24px] h-[25px]' />
               </div>
             </a>
 
-            <a href="" target='_blank' className='text-white'>
+            <a href="" target='_blank' className='text-white' data-aos="fade-up"
+              data-aos-duration="1700">
               <div className='bg-gradient-to-l from-[#F8B515] hover:from-[#f8b415ec] to-[#0FC1C7] hover:to-[#0fc1c7e8] p-2 xl:p-[15px] 2xl:p-4 rounded-full duration-300'>
-                <img src={icon2} alt="Image" className='w-[26px]'/>
+                <img src={icon2} alt="Image" className='w-[26px]' />
               </div>
             </a>
           </div>
