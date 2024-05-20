974ba08de00592f13b2a65683fce9d4fc26ea0cc
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-20 07:16:19 +0600

    aa

--- src/components/Sections/HowToBuy/HowToBuy.jsx
+++ src/components/Sections/HowToBuy/HowToBuy.jsx
@@ -25,7 +25,7 @@ const HowToBuy = () => {
             <div className='2xl:ml-[4px] xl:ml-[4px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
               <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-[10%]' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[42%] xl:w-[41%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+              <div className='2xl:w-[44%] xl:w-[41%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
                   <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Create <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>Wallet</span></p>
                   <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Download metamask from the app store or google play store for free. Desktoo users, download the aocale chrome I</p>
@@ -42,7 +42,7 @@ const HowToBuy = () => {
             <div className='2xl:mt-[-170px] xl:mt-[-110px] 2xl:ml-[860px] xl:ml-[660px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
               <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-[24%]' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[100%] xl:w-[94%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+              <div className='2xl:w-[100%] xl:w-[99%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
                   <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Get some <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>BNB</span></p>
                   <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Have BNB in your wallet to switch to skillSolona. If you don't have any BNB, you con buy directly on metamosk, transfer from another wollet, or buy on another exchange and send it to your wollet.</p>
@@ -67,18 +67,18 @@ const HowToBuy = () => {
 
 
             {/* ----------- card 3 --------- */}
-            <div className='2xl:mt-[-830px] 2xl:ml-[4px] 2xl:space-y-[-10px]'>
-              <img className='2xl:ml-[240px] 2xl:w-auto' src={cardImage} alt="image" />
+            <div className='2xl:mt-[-830px] xl:mt-[-630px] 2xl:ml-[4px] xl:ml-[4px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
+              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-[10%]' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[44%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
-                <div className='bg-[#18534f] 2xl:p-[40px] rounded-[30px]'>
-                  <p className='text-[#FFF] text-[28px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Go to <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>PancakeSwap</span></p>
-                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:leading-[28.8px] xl:tracking-[0.54px]'>Connect to PancakeSwap. Go to PancakeSwap in google chrome or on the browser inside wour lictamark ago. Connect your wallet. Paste the sKillSolana BNB token Smart Contract address (0x4eCd50E&8507204D487555c59851755EF9fFAF9) into PoncokeSwoo, select TWEETY. and confirm. When. metamonk promote uou for a wollet sanature, sion.</p>
+              <div className='2xl:w-[44%] xl:w-[41%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+                <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
+                  <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Go to <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>PancakeSwap</span></p>
+                  <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Connect to PancakeSwap. Go to PancakeSwap in google chrome or on the browser inside wour lictamark ago. Connect your wallet. Paste the sKillSolana BNB token Smart Contract address (0x4eCd50E&8507204D487555c59851755EF9fFAF9) into PoncokeSwoo, select TWEETY. and confirm. When. metamonk promote uou for a wollet sanature, sion.</p>
                 </div>
               </div>
               {/* card shadow image  */}
               <div>
-                <img className='2xl:mt-[-220px] 2xl:ml-[440px] 2xl:w-auto' src={cardImageShadow} alt="image" />
+                <img className='2xl:mt-[-220px] xl:mt-[-250px] 2xl:ml-[440px] xl:ml-[230px] 2xl:w-auto xl:w-[22%]' src={cardImageShadow} alt="image" />
               </div>
 
             </div>
@@ -88,18 +88,18 @@ const HowToBuy = () => {
 
 
             {/* ----------- card 4 --------- */}
-            <div className='2xl:mt-[-170px] 2xl:ml-[860px] 2xl:space-y-[-10px]'>
-              <img className='2xl:ml-[240px] 2xl:w-auto' src={cardImage} alt="image" />
+            <div className='2xl:mt-[-170px] xl:mt-[-170px] 2xl:ml-[860px] xl:ml-[660px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
+              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-auto' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[100%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
-                <div className='bg-[#18534f] 2xl:p-[40px] rounded-[30px]'>
-                  <p className='text-[#FFF] text-[28px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Swap BNB for <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>KillSolana</span></p>
-                  <p className='2xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:leading-[28.8px] xl:tracking-[0.54px]'>Switch BNB for #KillSolana. We have ZERO toxes so you don't need to worry about buying with a specific slippage, although you may need to use slippage during times of market volatility. HOLD KillSolana BNB for 1000x ATH Growth + s</p>
+              <div className='2xl:w-[100%] xl:w-[99%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+                <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
+                  <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Swap BNB for <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>KillSolana</span></p>
+                  <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Switch BNB for #KillSolana. We have ZERO toxes so you don't need to worry about buying with a specific slippage, although you may need to use slippage during times of market volatility. HOLD KillSolana BNB for 1000x ATH Growth + s</p>
                 </div>
               </div>
               {/* card shadow image  */}
               <div>
-                <img className='2xl:mt-[-220px] 2xl:ml-[415px] 2xl:w-auto' src={cardImageShadow} alt="image" />
+                <img className='2xl:mt-[-220px] xl:mt-[-220px] 2xl:ml-[415px] xl:ml-[290px] 2xl:w-auto xl:w-auto' src={cardImageShadow} alt="image" />
               </div>
 
             </div>
