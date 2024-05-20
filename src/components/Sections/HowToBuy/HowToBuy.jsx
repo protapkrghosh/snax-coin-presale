4b930b8c2522a166360ca854724b559f13ccc4c2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-20 06:19:55 +0600

    aa

--- src/components/Sections/HowToBuy/HowToBuy.jsx
+++ src/components/Sections/HowToBuy/HowToBuy.jsx
@@ -23,9 +23,9 @@ const HowToBuy = () => {
 
             {/* ----------- card 1 --------- */}
             <div className='2xl:ml-[4px] xl:ml-[4px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
-              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-auto' src={cardImage} alt="image" />
+              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-[10%]' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[42%] xl:w-[42%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+              <div className='2xl:w-[42%] xl:w-[41%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
                   <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Create <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>Wallet</span></p>
                   <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Download metamask from the app store or google play store for free. Desktoo users, download the aocale chrome I</p>
@@ -33,16 +33,16 @@ const HowToBuy = () => {
               </div>
               {/* card shadow image  */}
               <div>
-                <img className='2xl:mt-[-220px] xl:mt-[-220px] 2xl:ml-[410px] xl:ml-[410px] 2xl:w-auto xl:w-auto' src={cardImageShadow} alt="image" />
+                <img className='2xl:mt-[-220px] xl:mt-[-185px] 2xl:ml-[410px] xl:ml-[290px] 2xl:w-auto xl:w-[16%]' src={cardImageShadow} alt="image" />
               </div>
 
             </div>
 
             {/* ----------- card 2 --------- */}
-            <div className='2xl:mt-[-170px] xl:mt-[-170px] 2xl:ml-[860px] xl:ml-[860px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
-              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-auto' src={cardImage} alt="image" />
+            <div className='2xl:mt-[-170px] xl:mt-[-110px] 2xl:ml-[860px] xl:ml-[660px] 2xl:space-y-[-10px] xl:space-y-[-10px]'>
+              <img className='2xl:ml-[240px] xl:ml-[240px] 2xl:w-auto xl:w-[24%]' src={cardImage} alt="image" />
 
-              <div className='2xl:w-[100%] xl:w-[100%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
+              <div className='2xl:w-[100%] xl:w-[94%] p-[1px] bg-gradient-to-l from-[#F8B515] to-[#0FC1C7] rounded-[30px] w-fit'>
                 <div className='bg-[#18534f] 2xl:p-[40px] xl:p-[30px] rounded-[30px]'>
                   <p className='text-[#FFF] 2xl:text-[28px] xl:text-[24px] xl:leading-[33.6px] xl:tracking-[0.84px]'>Get some <span className='bg-clip-text text-transparent bg-gradient-to-l from-[#F8B515] to-[#0FC1C7]'>BNB</span></p>
                   <p className='2xl:mt-[15px] xl:mt-[15px] text-[#E7D7CB] 2xl:text-[18px] xl:text-[14px] 2xl:leading-[28.8px] 2xl:tracking-[0.54px]'>Have BNB in your wallet to switch to skillSolona. If you don't have any BNB, you con buy directly on metamosk, transfer from another wollet, or buy on another exchange and send it to your wollet.</p>
@@ -50,7 +50,7 @@ const HowToBuy = () => {
               </div>
               {/* card shadow image  */}
               <div>
-                <img className='2xl:mt-[-220px] xl:mt-[-220px] 2xl:ml-[415px] xl:ml-[415px] 2xl:w-auto xl:w-auto' src={cardImageShadow} alt="image" />
+                <img className='2xl:mt-[-220px] xl:mt-[-220px] 2xl:ml-[415px] xl:ml-[255px] 2xl:w-auto xl:w-auto' src={cardImageShadow} alt="image" />
               </div>
 
             </div>
@@ -58,8 +58,8 @@ const HowToBuy = () => {
 
 
             {/* ---------------- main images ---------------- */}
-            <div className='2xl:mt-[-540px] xl:mt-[-540px]'>
-              <img className='mx-auto 2xl:pl-[15px] xl:pl-[15px] 2xl:w-[50%] xl:w-[50%]' src={howToImg} alt="image" />
+            <div className='2xl:mt-[-540px] xl:mt-[-440px]'>
+              <img className='mx-auto 2xl:pl-[15px] 2xl:w-[50%] xl:w-[50%]' src={howToImg} alt="image" />
             </div>
 
 
